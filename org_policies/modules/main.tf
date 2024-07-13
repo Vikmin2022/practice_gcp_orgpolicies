802bfc2e2b63450ec5d8d6935dba028e8061827e
Viktoriia Minosian <116222974+Vikmin2022@users.noreply.github.com>
2024-07-13 17:50:49 -0500

    try

--- org_policies/modules/main.tf
+++ org_policies/modules/main.tf
@@ -1,12 +1,12 @@
-resource  "google_project_organization_policy" "deny_public_bucket"{
-     project =  "My First Project" # "My First Project"
-    constraint = "constraints/storage.publocAccessPrevention"
+# resource  "google_project_organization_policy" "deny_public_bucket"{
+#      project =  "zeta-handler-369021" # "My First Project"
+#     constraint = "constraints/storage.publocAccessPrevention"
 
-    boolean_policy {
-        enforced =true
-    }
+#     boolean_policy {
+#         enforced =true
+#     }
 
-}
+# }
 
 
  
--- org_policies/provider.tf
+++ org_policies/provider.tf
@@ -18,7 +18,6 @@ terraform {
 provider "google" {
   project     = "TF_VAR_project_id"
   region      = "us-central1"
-  credentials = base64decode(env.GCP_SA_KEY_JSONa)
 }
 
 
