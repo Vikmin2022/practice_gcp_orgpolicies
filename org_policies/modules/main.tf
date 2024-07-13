3bd5a92f1b2f55f31e8c4054638a4967a09d4d9b
Viktoriia Minosian <116222974+Vikmin2022@users.noreply.github.com>
2024-07-13 13:10:01 -0500

    test

---
+++ org_policies/modules/main.tf
@@ -0,0 +1,10 @@
+resource  "google_project_organization_policy" "deny_public_bucket"{
+    project = "My First Project"
+    constraint = "constraints/storage.publocAccessPrevention"
+
+    boolean_policy {
+        enforced =true
+    }
+
+}
+ 
